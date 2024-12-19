f26a13a0ce366f6988ea9abe62b693c4f55e5d2e
karan190806 <kc879022@gmail.com>
2024-12-19 22:25:01 +0530

    add config.py file

---
+++ src/config/config.py
@@ -0,0 +1,17 @@
+'''
+Author : Karan Chauhan
+github : @Karan-Chauhan19
+Organization : L.J University
+'''
+
+import torch
+import subprocess
+
+class Config:
+    def __init__(self):
+        pass
+
+    def get_config(self) -> None:
+        """
+        This function is check whether GPU are avilable or not 
+        """
