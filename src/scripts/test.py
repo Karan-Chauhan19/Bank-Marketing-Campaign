99f670ef084bd781da2e87f8cd3be1c32d99deb9
karan190806 <kc879022@gmail.com>
2024-12-23 17:12:10 +0530

    added test file

--- src/scripts/test.py
+++ src/scripts/test.py
@@ -3,3 +3,13 @@ Author : Karan Chauhan
 github : @Karan-Chauhan19
 Organization : L.J University
 '''
+
+from scripts.train import *
+
+class TestModel :
+    def __init__(self) :
+        self.pred = None
+    
+    def test(self) :
+        self.y_pred = TrainModel().model.predict(TrainModel().X_test)
+
